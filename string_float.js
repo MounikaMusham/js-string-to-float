6591b14997f2c2d5813fa44052560f17a0344823
mounika <mounikamusham12@gmail.com>
2021-12-13 18:03:03 +0530

    string-float

---
+++ string_float.js
@@ -0,0 +1,11 @@
+var str="10";
+console.log(typeof(str));
+console.log(parseFloat(str));
+console.log(typeof(parseFloat(str)));
+
+console.log("Another Example of \"string to float\" ");
+
+var strNum="100";
+console.log(typeof(strNum));
+console.log(parseFloat(strNum));
+console.log(typeof(parseFloat(strNum)));
